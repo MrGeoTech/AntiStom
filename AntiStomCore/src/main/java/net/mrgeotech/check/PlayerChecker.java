eb5ed6d00d8b7a72b90395e276a7ad59ebe2203e
MrGeoTech <themrgeotech@gmail.com>
2022-04-19 17:22:42 -0500

    Adding the (hopefully) last few checks to PlayerSnapshot

---
+++ AntiStomCore/src/main/java/net/mrgeotech/check/PlayerChecker.java
@@ -0,0 +1,11 @@
+package net.mrgeotech.check;
+
+import net.mrgeotech.data.PlayerSnapshot;
+
+public class PlayerChecker {
+
+    public static void checkMovement(PlayerSnapshot previous, PlayerSnapshot current) {
+
+    }
+
+}
