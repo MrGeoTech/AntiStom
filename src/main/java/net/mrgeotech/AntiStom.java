81567ba4d8681316b0f461cb98a407a2bea5a70b
MrGeoTech <themrgeotech@gmail.com>
2022-04-19 14:20:24 -0500

    Initial commit

---
+++ src/main/java/net/mrgeotech/AntiStom.java
@@ -0,0 +1,17 @@
+package net.mrgeotech;
+
+import net.minestom.server.extensions.Extension;
+
+public class AntiStom extends Extension {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void terminate() {
+
+    }
+
+}
